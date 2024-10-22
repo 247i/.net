ebbbec13a741b7cd65ed1c2c11c2e7108babcd5b
தமிழ் நேரம் <anishprabu.t@gmail.com>
2024-10-22 23:32:19 +0530

    புள்ளிப்பிணை\புள்ளிப்பிணை64

--- packs/Microsoft.Android.Sdk.Windows/33.0.95/tools/binutils/bin/arm-linux-androideabi-as.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set BINARIES_DIRECTORY=%~dp0
-
-set "BINARIES_DIRECTORY=" && "%BINARIES_DIRECTORY%as.exe" @gas-arch=arm-linux-androideabi-as %*
-if not ERRORLEVEL 0 exit /b %errorlevel%
--- packs/Microsoft.Maui.Sdk/8.0.82/README.md
+++
@@ -1,1 +0,0 @@
-.NET Multi-platform App UI (.NET MAUI) SDK. Please do not reference this package directly.
